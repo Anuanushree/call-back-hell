9b83d896de1ebaa76d39b77598cf7574742f8cc1
Anuanushree <anuabi2408@gmail.com>
2023-05-29 11:49:17 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,117 @@
+
+c = 10;
+function function1(callback) {
+    setTimeout(() => {
+        countdown.innerHTML = c;
+        c--;
+        callback();
+    }, 1000)
+}
+function function1(callback) {
+    setTimeout(() => {
+        countdown.innerHTML = c;
+        c--;
+        callback();
+    }, 1000)
+}
+function function2(callback) {
+    setTimeout(() => {
+        countdown.innerHTML = c;
+        c--;
+        callback();
+    }, 1000)
+}
+function function3(callback) {
+    setTimeout(() => {
+        countdown.innerHTML = c;
+        c--;
+        callback();
+    }, 900)
+}
+function function4(callback) {
+    setTimeout(() => {
+        countdown.innerHTML = c;
+        c--;
+        callback();
+    }, 900)
+}
+function function5(callback) {
+    setTimeout(() => {
+        countdown.innerHTML = c;
+        c--;
+        callback();
+    },900)
+}
+function function6(callback) {
+    setTimeout(() => {
+        countdown.innerHTML = c;
+        c--;
+        callback();
+    }, 500)
+}
+function function7(callback) {
+    setTimeout(() => {
+        countdown.innerHTML = c;
+        c--;
+        callback();
+    }, 900)
+}
+function function8(callback) {
+    setTimeout(() => {
+        countdown.innerHTML = c;
+        c--;
+        callback();
+    }, 900)
+}
+function function9(callback) {
+    setTimeout(() => {
+        countdown.innerHTML = c;
+        c--;
+        callback();
+    }, 900)
+}
+function function10(callback) {
+    setTimeout(() => {
+        countdown.innerHTML = c;
+        c--;
+        callback();
+    }, 900)
+}
+function function11(callback) {
+    setTimeout(() => {
+        countdown.innerHTML = c;
+        c--;
+        callback();
+    }, 900)
+}
+
+
+function1(function () {
+    function2(function () {
+        function3(function () {
+            function4(function () {
+                function5(function () {
+                    function6(function () {
+                        function7(function () {
+                            function8(function () {
+                                function9(function () {
+                                    function10(function () {
+                                        function11(function () {
+                                            countdown.innerHTML = "HAPPY INDEPENDENCE DAY";
+                                            countdown.style.color = "red";
+                                    
+                                              let s = document.getElementById('container');
+                                             s.style.backgroundImage = "url('R.gif')";
+                                            s.classList.add('img-fluid')
+                                        })
+
+                                    })
+                                })
+                            })
+                        })
+                    })
+                })
+            })
+        })
+    })
+})
